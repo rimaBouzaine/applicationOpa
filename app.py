2713395420e8372dbed2bad3dc63ececdece6f00
EC2 Default User <ec2-user@ip-172-31-92-206.ec2.internal>
2024-04-18 15:42:21 +0000

    app.py

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ def get_services():
         result = subprocess.run(['kubectl', 'get', 'svc'], capture_output=True, text=True)
         # Vérifie le code de sortie de la commande
         if result.returncode == 0:
+            print("Bonjour")
             # Si la commande a réussi, retourne la sortie en tant que réponse JSON
             return jsonify({'output': result.stdout})
         else:
+            print("ERRORRRRR")
             # Si la commande a échoué, retourne un message d'erreur
             return jsonify({'error': result.stderr}), 500
     except Exception as e:
         # En cas d'erreur, retourne un message d'erreur interne du serveur
         return jsonify({'error': str(e)}), 500
-
 if __name__ == '__main__':
    # app.run(host='0.0.0.0', port=5000)
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
-
