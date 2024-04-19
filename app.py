880c6f4716c9eb7c410f14fa37a36277f9b8b65f
EC2 Default User <ec2-user@ip-172-31-92-206.ec2.internal>
2024-04-19 09:56:08 +0000

    last version

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ app.config['LABEL_NAME'] = 'my-flask-app'
 def get_services():
     try:
         # Exécute la commande `kubectl get svc` en tant que sous-processus
-        result = subprocess.run(['/usr/bin/kubectl', 'get', 'svc'], capture_output=True, text=True)
+        result = subprocess.run(['kubectl', 'get', 'svc'], capture_output=True, text=True)
         # Vérifie le code de sortie de la commande
         if result.returncode == 0:
             print("Bonjour")
             # Si la commande a réussi, retourne la sortie en tant que réponse JSON
-            return jsonify({'output': result.stdout})
+            return jsonify({'your-output': result.stdout})
         else:
             print("ERRORRRRR")
             # Si la commande a échoué, retourne un message d'erreur
-            return jsonify({'error': result.stderr}), 500
+            return jsonify({'your-custom-error': result.stderr}), 500
     except Exception as e:
         # En cas d'erreur, retourne un message d'erreur interne du serveur
         return jsonify({'error': str(e)}), 500
